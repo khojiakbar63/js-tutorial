59160916fe10b7f3ffd3f060ffaf53bcc978e62d
khojiakbar63 <abdulaxatovx7@gmail.com>
2024-03-29 10:30:13 +0500

    js

---
+++ index.js
@@ -0,0 +1,256 @@
+// object
+/*
+
+let person = {
+   id: '213122313',
+   name: 'Khojiakbar',
+   surname: 'Abdulakhatov',
+   age: 24,
+   gender: 'male',
+   isMarried: true,
+   hobbies: ['walking alone', 'solving cubes rube'],
+   skills: ['HTML', 'CSS', 'TAILWINDCSS', 'SASS', 'BOOTSTRAP'],
+   profession: 'student',
+   address: {
+      country: "Uzbekistan",
+      region: "Tashkent",
+      district: "Akkurgan",
+      street: "Do'rmon",
+      homeNumber: 26,
+   },
+   contact:{
+      telNumber: "+998900241098",
+      email: "khojiakbar7796@mail.ru",
+      telegram: "@Xojiakbarrr",
+      instagram: "khojiakbar1199",
+      githubId: "khojiakbar63"
+   }
+
+}
+console.log(person.address.country); */
+
+// Intger 2
+// let weight = +prompt('Weight= ');
+// let remainder = weight % 1000;
+// let tonna = weight - remainder
+
+// alert(tonna + " tonna.")
+
+// Integer 3
+// let fileCapasityByte = +prompt("Enter file capacity: ");
+// let remainder = (fileCapasityByte % 1024);
+// let fileFullByte = (fileCapasityByte - remainder) / 1024;
+
+// alert("Remainder: " + remainder + " kilo byte.")
+// alert("Full byte: " + fileFullByte + " byte.")
+
+// Integer 4
+// alert("A kesmada B kesmani necha martta joylashtirish mumkinligini aniqlab beradi");
+// let A = +prompt("A: ");
+// let B = +prompt("B: ");
+
+// if (A > B){
+//    let remainder = (A % B);
+//    let times = (A - remainder) / B;
+//    alert("Qoldiq: " + remainder)
+//    alert("A kesmada B kesma " + times + " ta joylashadi.")
+// } else{
+//    alert("B is bigger than A!")
+// }
+
+// Integer 5
+// alert("A kesmada B kesmani necha martta joylashtirish mumkinligini aniqlab beradi va A kesmada B kesmani joylashmagan qismini aniqlovchi dastur.");
+// let A = +prompt("A: ");
+// let B = +prompt("B: ");
+
+// if (A > B){
+//    let remainder = (A % B);
+//    let times = (A - remainder) / B;
+//    alert("A kesmada B kesma " + times + " ta joylashtirish mumkin.")
+//    alert("A kesmada B kesmani joylashmagan qismi : " + remainder + " ta.")
+// } else{
+//    alert("B is bigger than A!")
+// }
+
+// Integer 6
+
+// let threeDigitNumber = prompt("Enter two digit number: ")*1;
+// let birlik = (threeDigitNumber % 10);
+// let onlik = (threeDigitNumber - birlik) /10;
+
+// alert("O'nlik: " + onlik)
+// alert("Birlik: " + birlik)
+
+
+// Integer 7
+
+// let threeDigitNumber = prompt("Enter two digit number: ")*1;
+// let birlik = (threeDigitNumber % 10);
+// let onlik = (threeDigitNumber - birlik) /10;
+// let sum = onlik + birlik
+
+// alert("O'nlik: " + onlik);
+// alert("Birlik: " + birlik);
+// alert("Sum of 2 digits is : " + sum + ".");
+
+
+// Integer 8
+
+// let threeDigitNumber = prompt("Enter two digit number: ")*1;
+// let birlik = (threeDigitNumber % 10);
+// let onlik = (threeDigitNumber - birlik) /10;
+// let sum = onlik + birlik
+
+// alert("O'nlik: " + onlik);
+// alert("Birlik: " + birlik);
+
+// alert("Numbers changed: " + birlik + onlik);
+
+// Integer 9
+
+// let threeDigitNumber = prompt("Enter three digit number: ")*1;
+// let remainderOfYuzlik = threeDigitNumber % 100;
+// let yuzlik = (threeDigitNumber - remainderOfYuzlik) / 100;
+// let remainderOfOnlik = remainderOfYuzlik % 10;
+// let onlik = (remainderOfYuzlik - remainderOfOnlik) / 10;
+// let birlik = remainderOfOnlik % 10;
+
+
+// alert("Yuzlik: " + yuzlik);
+// alert("O'nlik: " + onlik);
+// alert("Birlik: " + birlik);
+
+// Integer 10
+
+// let threeDigitNumber = prompt("Enter three digit number: ")*1;
+// let remainderOfYuzlik = threeDigitNumber % 100;
+// let yuzlik = (threeDigitNumber - remainderOfYuzlik) / 100;
+// let remainderOfOnlik = remainderOfYuzlik % 10;
+// let onlik = (remainderOfYuzlik - remainderOfOnlik) / 10;
+// let birlik = remainderOfOnlik % 10;
+
+
+// alert("Birlik: " + birlik);
+// alert("O'nlik: " + onlik);
+// alert("Yuzlik: " + yuzlik);
+
+// Integer 11 ????????????
+
+// let threeDigitNumber = Number(prompt("Enter three digit number: "));
+// let remainderOfYuzlik = threeDigitNumber % 100;
+// let yuzlik = (threeDigitNumber - remainderOfYuzlik) / 100;
+// let remainderOfOnlik = remainderOfYuzlik % 10;
+// let onlik = (remainderOfYuzlik - remainderOfOnlik) / 10;
+// let birlik = remainderOfOnlik % 10;
+
+// alert("Uchxonalik sonlar yig'indisi: " + birlik + onlik + yuzlik);
+
+// Integer 12
+
+// let threeDigitNumber = Number(prompt("Enter three digit number: "));
+// let remainderOfYuzlik = threeDigitNumber % 100;
+// let yuzlik = (threeDigitNumber - remainderOfYuzlik) / 100;
+// let remainderOfOnlik = remainderOfYuzlik % 10;
+// let onlik = (remainderOfYuzlik - remainderOfOnlik) / 10;
+// let birlik = remainderOfOnlik % 10;
+
+// alert("Uchxonalik sonlar teskari tartibda: " + birlik + onlik + yuzlik);
+
+                     //**** */ HOMEWORK /* ******//
+
+
+// Integer 14
+
+// let threeDigitNumber = Number(prompt("Enter three digit number: "));
+// let remainderOfYuzlik = threeDigitNumber % 100;
+// let yuzlik = (threeDigitNumber - remainderOfYuzlik) / 100;
+// let remainderOfOnlik = remainderOfYuzlik % 10;
+// let onlik = (remainderOfYuzlik - remainderOfOnlik) / 10;
+// let birlik = remainderOfOnlik % 10;
+
+// alert("Uchxonalik sonlar teskari tartibda: " + birlik + yuzlik + onlik);
+
+// Integer 15
+
+
+// let threeDigitNumber = Number(prompt("Enter three digit number: "));
+// let remainderOfYuzlik = threeDigitNumber % 100;
+// let yuzlik = (threeDigitNumber - remainderOfYuzlik) / 100;
+// let remainderOfOnlik = remainderOfYuzlik % 10;
+// let onlik = (remainderOfYuzlik - remainderOfOnlik) / 10;
+// let birlik = remainderOfOnlik % 10;
+
+// alert("O'nliklar yuzliklar bilan almashtirildi: " + onlik + yuzlik + birlik );
+
+// Integer 16
+
+
+// let threeDigitNumber = Number(prompt("Enter three digit number: "));
+// let remainderOfYuzlik = threeDigitNumber % 100;
+// let yuzlik = (threeDigitNumber - remainderOfYuzlik) / 100;
+// let remainderOfOnlik = remainderOfYuzlik % 10;
+// let onlik = (remainderOfYuzlik - remainderOfOnlik) / 10;
+// let birlik = remainderOfOnlik % 10;
+
+// alert("O'nliklar birliklar bilan almashtirildi: " + yuzlik + birlik + onlik);
+
+// Integer 17, 18
+
+// let number = +prompt("Enter number(number > 999)");
+
+// let qoldiqOfminglik = (number % 1000); //minglik qoldiq
+// let qoldiqOfyuzlik = number % 100 //yuzlik qoldiq
+// let minglik = (number - qoldiqOfminglik) / 1000;
+// let yuzlik = (((number - 1000) - qoldiqOfyuzlik)) /100;
+// alert("Yuzlik: " + yuzlik)
+// alert("Minklik: " + minglik)
+
+
+// Integer 19
+
+// let N = +prompt("Kun boshidan qancha minut otganini aniqlang(secund kiriting): ");
+// let qoldiq = N % 60;
+// let M = (N - qoldiq) / 60;
+
+// alert("Kun boshidan " + M + " daqiqa minut otdi.");
+
+
+// Integer 20
+
+// let N = +prompt("Kun boshidan qancha soat otganini aniqlang(secund kiriting): ");
+// let qoldiq = N % 3600;
+// let S = (N - qoldiq) / 3600;
+
+// alert("Kun boshidan " + S + " soat otdi.");
+
+
+// Integer 21
+
+// let N = +prompt("Kun boshidan qancha minut va sekund otganini aniqlang(secund kiriting): ");
+// let qoldiq = N % 60;
+// let M = (N - qoldiq) / 60;
+
+// alert("Kun boshidan " + M + " daqiqa " + qoldiq + " sekund otdi.");
+
+// Integer 22
+
+// let N = +prompt("Kun boshidan qancha soat va sekund otganini aniqlang(secund kiriting): ");
+// let qoldiq = N % 60;
+// let S = (N - qoldiq) / 3600;
+
+// alert("Kun boshidan " + S + " soat " + qoldiq + " sekund otdi.");
+
+// Integer 23
+
+// let N = +prompt("Kun boshidan qancha soat, daqiqa va sekund otganini aniqlang(secund kiriting): ");
+// let S = (N-N%3600)/3600;
+// let M = (N%3600-N%60)/60;
+// let Sek =N%60
+// alert("Kun boshidan " + S + " soat "+ M +" daqiqa " + Sek + " sekund otdi.");
+
+
+
+
+
+
+
